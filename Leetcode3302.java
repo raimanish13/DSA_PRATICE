53a8919ceed5f415b7f5eb5e399a1dde67c6cfa9
manishrai <manishkumarrai535@gmail.com>
2024-10-03 00:49:12 +0530

    Updated the solution

--- Leetcode3302.java
+++ Leetcode3302.java
@@ -3,7 +3,7 @@ class Leetcode3302 {
         int n=word1.length();
         int m=word2.length();
         int count=0;
-        int[] maximumSuffix=new int[n];
+        int[] maximumSuffix=new int[n+1];
         int i=n-1;
         int j=m-1;
         while(i>0 && j>0)
@@ -17,24 +17,45 @@ class Leetcode3302 {
             }
             else
             {
-                maximumSuffix[i]=count;
                 i--;
             }
         }
-        maximumSuffix[i]=count;
-        i=0,j=0,count=0,flag=0;
+        for(i=n-2;i>=0;i--)
+        {
+            maximumSuffix[i]=Math.max(maximumSuffix[i],maximumSuffix[i+1]);
+        }
+        int[] ans =new int[m];
+        int flag=0;
+        count=0;
+        i=0;j=0;
         while(i<n && j<m)
         {
-            if(word.charAt(i)==word2.charAt(j))
+            if(word1.charAt(i)==word2.charAt(j))
             {
                 ans[j]=i;
                 i++;
                 j++;
+                count++;
             }
-            else
-            {
-                if()
+            else{
+                int remainingLength=(m-j-1);
+                if(maximumSuffix[i+1]>=remainingLength && flag==0)
+                {
+                    ans[j]=i;
+                    i++;
+                    j++;
+                    flag=1;
+                    count++;
+                }else{
+                    i++;
+                }
             }
         }
+        if(count!=m)
+        {
+            return new int[0];
+        } 
+        
+        return ans;
     }
 }
